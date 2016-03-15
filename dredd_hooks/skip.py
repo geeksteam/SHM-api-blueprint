0471b33e92b0fedc46122dd2a73a5e8ce4d7cffc
Maksim Kalashnikov <kalashnikovm@mail.ru>
2016-03-15 17:36:50 +0200

    fix

--- dredd_hooks/skip.py
+++ dredd_hooks/skip.py
@@ -5,7 +5,12 @@ import dredd_hooks as hooks
 def skip_test(transaction):
   transaction['skip'] = True
 
-# Skip IMAGE/PNG body
+# Skip HOTP code confirm
+@hooks.before("Two step authorization > Check for correct app binding > Code correct")
+def skip_test(transaction):
+  transaction['skip'] = True
+
+# Skip HOTP QR IMAGE body
 @hooks.before_validation("Two step authorization > Generate QR > Getting QR image with code")
 def skip_body(transaction):
     transaction['real']['body'] = ''
