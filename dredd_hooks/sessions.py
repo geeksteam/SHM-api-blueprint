b38a26d3401fa8fa8b37edd1fee811f5da06d3e3
Maksim Kalashnikov <kalashnikovm@mail.ru>
2016-03-15 13:53:47 +0200

    hooks added

---
+++ dredd_hooks/sessions.py
@@ -0,0 +1,16 @@
+import json
+import dredd_hooks as hooks
+
+stash = {}
+
+# hook to retrieve session on a login
+@hooks.after('Panel Authorization > Login success > POST')
+def stash_session_id(transaction):
+  parsed_body = json.loads(transaction['real']['body'])
+  stash['token'] = parsed_body['sessionID']
+
+# hook to set the session cookie in all following requests
+@hooks.before_each
+def add_session_cookie(transaction):
+  if 'token' not in stash:
+    transaction['request']['headers']['Cookie'] = "sessionID=" + stash['token']
