d7d865360289b3032352095d39748c3f560a8a3f
Maksim Kalashnikov <kalashnikovm@mail.ru>
2016-03-15 17:38:52 +0200

    fix skip

--- dredd_hooks/skip.py
+++ dredd_hooks/skip.py
@@ -2,15 +2,15 @@ import dredd_hooks as hooks
 
 # Skip HOTP codes
 @hooks.before("Panel Authorization > User login > Login with HOTP success")
-def skip_test(transaction):
+def skip_login_hotp(transaction):
   transaction['skip'] = True
 
 # Skip HOTP code confirm
 @hooks.before("Two step authorization > Check for correct app binding > Code correct")
-def skip_test(transaction):
+def skip_hotp_check_code(transaction):
   transaction['skip'] = True
 
 # Skip HOTP QR IMAGE body
 @hooks.before_validation("Two step authorization > Generate QR > Getting QR image with code")
-def skip_body(transaction):
+def skip_hotp_image(transaction):
     transaction['real']['body'] = ''
