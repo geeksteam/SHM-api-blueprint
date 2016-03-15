4113e4630b359cc9d0e1075b3448735f00b214cb
Maksim Kalashnikov <kalashnikovm@mail.ru>
2016-03-15 14:12:35 +0200

    commented out session

--- dredd_hooks/sessions.py
+++ dredd_hooks/sessions.py
@@ -1,20 +1,20 @@
-import json
-import dredd_hooks as hooks
+# import json
+# import dredd_hooks as hooks
 
-stash = {}
+# stash = {}
 
-# hook to retrieve session on a login
-@hooks.after('Panel Authorization > Login success > POST')
-def stash_session_id(transaction):
+# # hook to retrieve session on a login
+# @hooks.after('Panel Authorization > Login success > POST')
+# def stash_session_id(transaction):
 
-	parsed_body = json.loads(transaction['real']['body'])
-	stash['token'] = parsed_body['sessionID']
-	print 'Session hook'
+# 	parsed_body = json.loads(transaction['real']['body'])
+# 	stash['token'] = parsed_body['sessionID']
+# 	print 'Session hook'
 
-# hook to set the session cookie in all following requests
-@hooks.before_each
-def add_session_cookie(transaction):
-	print 'After each'
-	if 'token' not in stash:
-		transaction['request']['headers']['Cookie'] = "sessionID=" + stash['token']
-		
+# # hook to set the session cookie in all following requests
+# @hooks.before_each
+# def add_session_cookie(transaction):
+# 	print 'After each'
+# 	if 'token' not in stash:
+# 		transaction['request']['headers']['Cookie'] = "sessionID=" + stash['token']
+# 		
