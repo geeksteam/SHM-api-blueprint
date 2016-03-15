dd7d9f42b7d14996ccb76fb647918be5903cb3ac
Maksim Kalashnikov <kalashnikovm@mail.ru>
2016-03-15 15:20:35 +0200

    session save fix

--- dredd_hooks/sessions.py
+++ dredd_hooks/sessions.py
@@ -6,11 +6,10 @@ stash = {}
 # hook to retrieve session on a login
 @hooks.after('Panel Authorization > User login > Login success')
 def stash_session_id(transaction):
-	stash['sessCookie'] = transaction['real']['headers']['set-cookie']
+        stash['sessID'] = transaction['real']['headers']['set-cookie']
 
 # hook to set the session cookie in all following requests
 @hooks.before_each
 def add_session_cookie(transaction):
-	if 'sessCookie' not in stash:
-		transaction['request']['headers']['Cookie'] = stash['sessCookie']
-		
+        if 'sessID' in stash:
+                transaction['request']['headers']['Cookie'] = stash['sessID']
--- dredd_hooks/skip.py
+++ dredd_hooks/skip.py
@@ -3,3 +3,7 @@ import dredd_hooks as hooks
 @hooks.before("Panel Authorization > User login > Login with HOTP success")
 def skip_test(transaction):
   transaction['skip'] = True
+
+@hooks.before("Panel Authorization > User login > Login fails because HOTP wrong code")
+def skip_test(transaction):
+  transaction['skip'] = True
