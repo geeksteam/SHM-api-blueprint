b4016cc244f66a94223dcd0744a011b112e8ff00
Maksim Kalashnikov <kalashnikovm@mail.ru>
2016-03-16 14:44:10 +0200

    parsing by groups

--- dredd_hooks/sessions.py
+++ dredd_hooks/sessions.py
@@ -3,31 +3,39 @@ import dredd_hooks as hooks
 
 stash = {}
 
-# List of requests that must be from USER
-user_requests = [
-		'User defined settings > Change user password > *',
+# List of groups that must be run as USER
+user_group_requests = [
+        'User defined settings',
+    ]
 
-	]
+# List of requests that must be run from USER
+user_requests = []
 
 
 # Retrieve ROOT sessionID on a login
 @hooks.after('Panel Authorization > Root login > Root login success')
 def stash_root_session_id(transaction):
-        stash['root_sessID'] = transaction['real']['headers']['set-cookie']
+    stash['root_sessID'] = transaction['real']['headers']['set-cookie']
 
 # Retrieve USER sessionID on a login
 @hooks.after('Panel Authorization > User login > Login success')
 def stash_user_session_id(transaction):
-        stash['user_sessID'] = transaction['real']['headers']['set-cookie']
+    stash['user_sessID'] = transaction['real']['headers']['set-cookie']
 
 
 # Set the ROOT or USER session cookie in all requests
 @hooks.before_each
 def add_session_cookie(transaction):
-	# Check is transaction in user list
+    # Check is request GROUP in USER list
+    if transaction['origin']['resourceGroupName'] in user_group_requests and 'user_sessID' in stash:
+        transaction['request']['headers']['Cookie'] = stash['user_sessID']
+        return
+
+	# Check is request in USER list
 	if transaction['name'] in user_requests and 'user_sessID' in stash:
-                transaction['request']['headers']['Cookie'] = stash['user_sessID']
-    	# run it as root
-    	else:
-    		if 'root_sessID' in stash:
-                	transaction['request']['headers']['Cookie'] = stash['root_sessID']
+        transaction['request']['headers']['Cookie'] = stash['user_sessID']
+        return
+
+    # run it as ROOT by default
+    if 'root_sessID' in stash:
+        transaction['request']['headers']['Cookie'] = stash['root_sessID']
