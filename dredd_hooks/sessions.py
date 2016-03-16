4f15b16c5a7d9558916d1639eb5a97cf92e9c9eb
Maksim Kalashnikov <kalashnikovm@mail.ru>
2016-03-16 12:00:01 +0200

    session ip strict test

--- dredd_hooks/sessions.py
+++ dredd_hooks/sessions.py
@@ -6,7 +6,8 @@ stash = {}
 # hook to retrieve session on a login
 @hooks.after('Panel Authorization > User login > Login success')
 def stash_session_id(transaction):
-        stash['sessID'] = transaction['real']['headers']['set-cookie']
+        stash['sessID'] = 'VVzHIXanHmZPqDijtXLggaCs'
+        #stash['sessID'] = transaction['real']['headers']['set-cookie']
 
 # hook to set the session cookie in all following requests
 @hooks.before_each
