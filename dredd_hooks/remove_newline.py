9c722db294ad828e6cedbc48335924f97021972c
Maksim Kalashnikov <kalashnikovm@mail.ru>
2016-03-23 14:54:32 +0200

    trying to add new line after

--- dredd_hooks/remove_newline.py
+++ dredd_hooks/remove_newline.py
@@ -3,4 +3,4 @@ import dredd_hooks as hooks
 @hooks.before_each
 def remove_trailing_newline(transaction):
   if transaction['expected']['headers']['Content-Type'] == 'text/plain':
-    transaction['expected']['body'] = transaction['expected']['body'].rstrip()
+    transaction['expected']['body'] += '\n'
