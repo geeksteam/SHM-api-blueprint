982e5966632f0b30a95f6e9905bc8b87449fb411
Maksim Kalashnikov <kalashnikovm@mail.ru>
2016-03-30 12:14:31 +0300

    fix for no headres response

--- dredd_hooks/remove_newline.py
+++ dredd_hooks/remove_newline.py
@@ -3,5 +3,6 @@ import dredd_hooks as hooks
 # Removing new line bug
 @hooks.before_each
 def remove_trailing_newline(transaction):
-  if transaction['expected']['headers']['Content-Type'] == 'text/plain':
-    transaction['expected']['body'] = transaction['expected']['body'].rstrip()
+        if  'headers' in transaction['expected'] and 'Content-Type' in transaction['expected']['headers']:
+                if transaction['expected']['headers']['Content-Type'] == 'text/plain':
+                        transaction['expected']['body'] = transaction['expected']['body'].rstrip()
