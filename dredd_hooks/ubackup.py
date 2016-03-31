19b758948b3c0b5c02dbc4f9a6ef84e5dc1209ad
Max Kalashnikov <kalashnikovm@mail.ru>
2016-03-31 22:47:24 +0300

    fix for restore backup name

--- dredd_hooks/ubackup.py
+++ dredd_hooks/ubackup.py
@@ -1,22 +1,20 @@
 import json
+import sys
 import dredd_hooks as hooks
 
 # Local stash
-response_stash = {}
-last_backup_name = 'noBackup'
+last_backup_name = 'noBackupFound'
 
 # Retrieve last backup archive name
 @hooks.after('User Backups > List backups > List backups')
 def save_last_backup_name(transaction):
-            response_stash[transaction['name']] = transaction['real']
+			global last_backup_name
+			response_json = json.loads(transaction['real']['body'])
+			last_backup_name = response_json[0]['Name']
         
 # Set backup arch Name
 @hooks.before('User Backups > Restore backup > Restore backup')
-def set_last_backup_name(transaction):
-            
-            # parsed_body = json.loads(response_stash['User Backups > List backups > List backups'])
-            # last_backup_name = parsed_body[1]['Name']
-            
+def set_last_backup_name(transaction):            
             transaction['request']['headers']['Backup-name'] = last_backup_name
             transaction['request']['body'] = transaction['request']['body'].replace('$BACKUP_ARCHIVE_NAME',last_backup_name)
 		
