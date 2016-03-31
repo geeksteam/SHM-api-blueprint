9e2f8a78ddf17f2e7676c288365da0586ff2b2f2
Maksim Kalashnikov <kalashnikovm@mail.ru>
2016-03-31 16:20:15 +0300

    fix response stash

--- dredd_hooks/backup.py
+++ dredd_hooks/backup.py
@@ -5,21 +5,17 @@ import dredd_hooks as hooks
 response_stash = {}
 last_backup_name = 'noBackup'
 
-def save_response_to_stash(transaction):
-            # saving HTTP response to the stash
-            response_stash[transaction['name']] = transaction['real']
-
 # Retrieve last backup archive name
 @hooks.after('User Backups > List backups > List backups')
 def save_last_backup_name(transaction):
-            save_response_to_stash(transaction)
+            response_stash[transaction['name']] = transaction['real']
         
 # Set backup arch Name
 @hooks.before('User Backups > Restore backup > Restore backup')
 def set_last_backup_name(transaction):
             
             parsed_body = json.loads(response_stash['User Backups > List backups > List backups'])
-            last_backup_name = parsed_body[0]['Name']
+            last_backup_name = parsed_body[1]['Name']
             
             transaction['request']['headers']['Backup-name'] = last_backup_name
             transaction['request']['body'] = transaction['request']['body'].replace('$BACKUP_ARCHIVE_NAME',last_backup_name)
