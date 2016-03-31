1db817133bf0e7f48637d1ea96df769976fd2b0b
Maksim Kalashnikov <kalashnikovm@mail.ru>
2016-03-31 13:38:38 +0300

    var added

--- dredd_hooks/backup.py
+++ dredd_hooks/backup.py
@@ -2,17 +2,16 @@ import json
 import dredd_hooks as hooks
 
 # Local stash
-stash={}
-
+last_backup_name = 'noBackup'
 
 # Retrieve last backup archive name
 @hooks.after('User Backups > List backups > List backups')
 def get_last_backup_name(transaction):
             arch_list=json.loads(transaction['real']['body'])
-            stash['last_backup_name'] = arch_list[0]['Name']
+            last_backup_name = arch_list[0]['Name']
         
 # Set backup arch Name
 @hooks.before('User Backups > Restore backup > Restore backup')
 def set_last_backup_name(transaction):
-            transaction['request']['body'].replace('$BACKUP_ARCHIVE_NAME',stash['last_backup_name'])
+            transaction['request']['body'].replace('$BACKUP_ARCHIVE_NAME',last_backup_name)
 		
