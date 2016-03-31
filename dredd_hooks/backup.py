f2eab3249092afaf182d71c2b8d272e54e01d5ef
Maksim Kalashnikov <kalashnikovm@mail.ru>
2016-03-31 16:23:56 +0300

    skip set resp for now

--- dredd_hooks/backup.py
+++ dredd_hooks/backup.py
@@ -14,8 +14,8 @@ def save_last_backup_name(transaction):
 @hooks.before('User Backups > Restore backup > Restore backup')
 def set_last_backup_name(transaction):
             
-            parsed_body = json.loads(response_stash['User Backups > List backups > List backups'])
-            last_backup_name = parsed_body[1]['Name']
+            # parsed_body = json.loads(response_stash['User Backups > List backups > List backups'])
+            # last_backup_name = parsed_body[1]['Name']
             
             transaction['request']['headers']['Backup-name'] = last_backup_name
             transaction['request']['body'] = transaction['request']['body'].replace('$BACKUP_ARCHIVE_NAME',last_backup_name)
