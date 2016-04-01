dd794508f61200ba00575bd141640f67c7d407d9
Maksim Kalashnikov <kalashnikovm@mail.ru>
2016-04-01 11:22:04 +0300

    fix

--- dredd_hooks/ubackup.py
+++ dredd_hooks/ubackup.py
@@ -13,8 +13,13 @@ def save_last_backup_name(transaction):
 			last_backup_name = response_json[0]['Name']
         
 # Set backup arch Name
-@hooks.before('User Backups > Restore backup > Restore backup', 'User Backups > Delete backup > Delete backup')
+@hooks.before('User Backups > Restore backup > Restore backup')
 def set_last_backup_name(transaction):            
             transaction['request']['headers']['Backup-name'] = last_backup_name
             transaction['request']['body'] = transaction['request']['body'].replace('$BACKUP_ARCHIVE_NAME',last_backup_name)
-		
+
+# Set backup arch Name
+@hooks.before('User Backups > Delete backup > Delete backup')
+def set_last_backup_name(transaction):            
+            transaction['request']['headers']['Backup-name'] = last_backup_name
+            transaction['request']['body'] = transaction['request']['body'].replace('$BACKUP_ARCHIVE_NAME',last_backup_name)
