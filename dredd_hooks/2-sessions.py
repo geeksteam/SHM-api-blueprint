ffc450cbfab0709b2ca4a9e19379f96b07685233
Maksim Kalashnikov <kalashnikovm@mail.ru>
2016-04-14 16:32:39 +0300

    fix func name

--- dredd_hooks/2-sessions.py
+++ dredd_hooks/2-sessions.py
@@ -59,7 +59,7 @@ def set_expected_error(transaction):
 
 ## Retrieve ROOT sessionID on a login
 @hooks.after('Create regularUser for tests > Auth as Root for testing user creation > Auth as Root for testing user creation')
-def stash_root_session_id(transaction):
+def stash_test_root_session_id(transaction):
         print 'Crete executed!'
         stash['root_sessID'] = transaction['real']['headers']['set-cookie']
         
