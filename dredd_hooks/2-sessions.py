75e0d1704b17601cf56195f25ccfeeff0dfd6f19
Maksim Kalashnikov <kalashnikovm@mail.ru>
2016-04-14 16:09:34 +0300

    fix sessionid catch

--- dredd_hooks/2-sessions.py
+++ dredd_hooks/2-sessions.py
@@ -58,6 +58,10 @@ def set_expected_error(transaction):
 ###
 
 ## Retrieve ROOT sessionID on a login
+@hooks.after('Create regularUser for tests > Auth as Root for testing user creation > Auth as Root for testing user creation')
+def stash_root_session_id(transaction):
+		stash['root_sessID'] = transaction['real']['headers']['set-cookie']
+        
 @hooks.after('Panel Authorization > Root login > Root login success')
 def stash_root_session_id(transaction):
 		stash['root_sessID'] = transaction['real']['headers']['set-cookie']
