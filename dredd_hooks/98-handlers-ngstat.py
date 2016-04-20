f1fac3e841865ea925670828e0556b72bf8b33c1
Maksim Kalashnikov <kalashnikovm@mail.ru>
2016-04-20 15:12:30 +0300

    fix url open while ng stat

--- dredd_hooks/98-handlers-ngstat.py
+++ dredd_hooks/98-handlers-ngstat.py
@@ -1,28 +1,14 @@
-import json
-import sys
-import requests
+import urllib2
 import time
 import dredd_hooks as hooks
 
-# Local stash
-last_backup_name = 'noBackupFound'
  
 # Set backup arch Name
 @hooks.before('Nginx URL statistics > Get all domains statistics > Get all domains statistics')
 def make_url_statistics(transaction):
-            if transaction['skip'] != True:
-                    link = "http://test.com/url_one"
-                    f = requests.get(link)
-                    link = "http://test.com/url_two"
-                    f = requests.get(link)
-                    link = "http://test.com/url_two"
-                    f = requests.get(link)
-                    link = "http://test.com/url_three"
-                    f = requests.get(link)
-                    link = "http://test.com/url_three"
-                    f = requests.get(link)
-                    link = "http://test.com/url_three"
-                    f = requests.get(link)
-                    # Wait for statistic flush to bucket
-                    time.sleep(63)
-                    
+        if transaction['skip'] != True:
+                response = urllib2.urlopen('http://test.com/url_one').read()
+                response = urllib2.urlopen('http://test.com/url_two').read()
+                response = urllib2.urlopen('http://test.com/url_three').read()
+                # Wait for statistic flush to bucket
+                time.sleep(63)
