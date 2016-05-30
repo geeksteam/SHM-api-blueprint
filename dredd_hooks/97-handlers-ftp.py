5b2252a6ab6c20967bf95ded1179aeb97e60c4aa
Max Kalashnikov <kalashnikovm@mail.ru>
2016-05-30 22:43:55 +0300

    fix ftp test names

--- dredd_hooks/97-handlers-ftp.py
+++ dredd_hooks/97-handlers-ftp.py
@@ -5,4 +5,10 @@ import dredd_hooks as hooks
 # Test FTP login as created account
 @hooks.before('FTP accounts > List FTP accounts > List FTP accounts')
 def check_ftp_account(transaction):
-				transaction['fail'] = "Cannot login to FTP account by test. Error:"
+        if transaction['skip'] != True:
+				try:
+						ftp = ftplib.FTP(server_ip)
+						ftp.login('FTP1', 'FTPPassword')
+						files = ftp.dir()
+				except ftplib.all_errors as e:
+						transaction['fail'] = "Cannot login to FTP account by test. Error: %s" % e
