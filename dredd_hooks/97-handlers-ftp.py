91341d7d745973049700fdb7467de457a8dfe663
Max Kalashnikov <kalashnikovm@mail.ru>
2016-05-30 23:46:29 +0300

    test ftp uploads added

--- dredd_hooks/97-handlers-ftp.py
+++ dredd_hooks/97-handlers-ftp.py
@@ -9,6 +9,11 @@ def check_ftp_account(transaction):
 				try:
 						ftp = ftplib.FTP(transaction['host'])
 						ftp.login(transaction['request']['headers']['Dredd-User']+'_FTP1', 'FTPPassword')
+						# List directory
 						files = ftp.dir()
+						# Upload file
+						myfile = open('/bin/sh', 'r')
+						final_file_name = 'test_bin_upload'
+						ftp.storbinary('STOR '+ final_file_name, myfile)
 				except ftplib.all_errors as e:
 						transaction['fail'] = "FTP account test by Dredd/Python FTPclient. Error: %s" % e
