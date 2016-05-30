c60e0a5b6d781ac13a04305e7382a28b93d20bbd
Max Kalashnikov <kalashnikovm@mail.ru>
2016-05-30 22:27:48 +0300

    test program fail

--- dredd_hooks/97-handlers-ftp.py
+++ dredd_hooks/97-handlers-ftp.py
@@ -2,13 +2,8 @@ from ftplib import FTP
 import time
 import dredd_hooks as hooks
 
-# Create url statistics
+# Test FTP login as created account
 @hooks.before('FTP accounts > List FTP accounts > List FTP accounts')
 def check_ftp_account(transaction):
         if transaction['skip'] != True:
-				try:
-						ftp = ftplib.FTP(server_ip)
-						ftp.login('FTP1', 'FTPPassword')
-						files = ftp.dir()
-				except ftplib.all_errors as e:
-						transaction['fail'] = "Cannot login to FTP account by test. Error: %s" % e
+				transaction['fail'] = "Cannot login to FTP account by test. Error:"
