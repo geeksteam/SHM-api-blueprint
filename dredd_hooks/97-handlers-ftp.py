31a644b26d8a2320a0abf09eac151cf3a9ec24ff
Max Kalashnikov <kalashnikovm@mail.ru>
2016-05-30 22:07:00 +0300

    ftp automatic client tester added

---
+++ dredd_hooks/97-handlers-ftp.py
@@ -0,0 +1,14 @@
+from ftplib import FTP
+import time
+import dredd_hooks as hooks
+
+# Create url statistics
+@hooks.after('FTP accounts > List FTP accounts > List FTP accounts')
+def check_ftp_account(transaction):
+        if transaction['skip'] != True:
+				try:
+						ftp = ftplib.FTP(server_ip)
+						ftp.login('FTP1', 'FTPPassword')
+						files = ftp.dir()
+				except ftplib.all_errors as e:
+						transaction['fail'] = "Cannot login to FTP account by test. Error: %s" % e
