5ab3ec1a9039c52bfa0ff333d05116ea3b0fe6bf
Maksim Kalashnikov <kalashnikovm@mail.ru>
2016-05-31 11:48:25 +0300

    dns testing added

---
+++ dredd_hooks/96-handlers-dns.py
@@ -0,0 +1,18 @@
+import time
+import socket
+import dns.resolver
+import dredd_hooks as hooks
+
+# Test DNS server response records
+@hooks.before('DNS Domains > List Domains records > List Domains records')
+def check_dns_records(transaction):
+        if transaction['skip'] != True:
+				# Set the DNS Server
+                resolver = dns.resolver.Resolver()
+                resolver.nameservers=[transaction['host']]
+                dnsresult = dnsresultresolver.query('testpdns.com', 'MX')
+                if len(dnsresult) < 1:
+                        transaction['fail'] = "DNS response from %s for testpdns.com MX has no records" % transaction['host']
+                print "Result of checking domains:"
+                for rdata in dnsresult:
+                        print rdata.target
--- dredd_hooks/98-handlers-ngstat.py
+++ dredd_hooks/98-handlers-ngstat.py
@@ -1,6 +1,7 @@
 from urllib2 import Request, urlopen, URLError, HTTPError
 import time
 import dredd_hooks as hooks
+from subprocess import Popen, PIPE
 
 # Run grab url function
 def run_url(urlname):
@@ -18,9 +19,15 @@ def run_url(urlname):
 @hooks.before('Nginx URL statistics > Get all domains statistics > Get all domains statistics')
 def make_url_statistics(transaction):
         if transaction['skip'] != True:
-        
+                # Check IP and test.com in /etc/hosts
+                process = Popen(["grep","-q",transaction['host'],"/etc/hosts"], stdout=PIPE)
+                process.communicate()    # execute it, the output goes to the stdout
+                exit_code = process.wait()    # when finished, get the exit code
+                if exit_code > 0:
+                        transaction['fail'] = "Python http client check by Dredd/Python error: Cannot find IP %s test.com in /etc/hosts" % transaction['host']
+                # Run server requests
                 run_url('http://test.com/url_one')
-                
+
                 run_url('http://test.com/url_two')
                 time.sleep(1)
                 run_url('http://test.com/url_two')
