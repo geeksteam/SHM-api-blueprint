d2619dbe2009e137901860d59ce2a20d08a0c8b6
Maksim Kalashnikov <kalashnikovm@mail.ru>
2016-05-31 12:30:11 +0300

    added dns resolver function

--- dredd_hooks/96-handlers-dns.py
+++ dredd_hooks/96-handlers-dns.py
@@ -1,31 +1,30 @@
 import time
 import socket
-import dns.resolver
 import dredd_hooks as hooks
+
+import dns.query
+import dns.resolver
+from dns.exception import DNSException
 
 # Test DNS server response records
 @hooks.before('DNS Domains > List Domains records > List Domains records')
 def check_dns_records(transaction):
         if transaction['skip'] != True:
-				# Set the DNS Server
-                resolver = dns.resolver.Resolver()
-                resolver.nameservers=[transaction['host']]
-                try:
-                        dnsresult = resolver.query.udp('testpdns.com', 'MX')
-                except dns.resolver.NXDOMAIN:
-                        transaction['fail'] = "DNS testing error: No such domain"
+        
+                # Set the DNS Server                
+                query = dns.message.make_query("testpdns.com", dns.rdatatype.MX)
+                response = dns.query.udp(query, transaction['host'])
+                if rcode != dns.rcode.NOERROR:
+                        if rcode == dns.rcode.NXDOMAIN:
+                                transaction['fail'] = "DNS testing error: No such domain"
+                                return
+                        else:
+                                transaction['fail'] = "DNS testing error: %s" % (dns.rcode.to_text(rcode))
+                                return
+                rrsets = response.answer
+                if len(rrsets) < 1:
+                        transaction['fail'] = "Empty records set got from NS"
                         return
-                except dns.resolver.NoAnswer:
-                        transaction['fail'] = "DNS testing error: No answer from dns %s" % transaction['host']
+                if rrsets[0][0].rdtype != dns.rdatatype.MX
+                        transaction['fail'] = "No MX record found"
                         return
-                except dns.resolver.Timeout:
-                        transaction['fail'] = "DNS testing error: Timed out while resolving %s" % transaction['host']
-                        return
-                except dns.exception.DNSException as e:
-                        transaction['fail'] = "DNS testing error: Unhandled exception: %s" % e.message
-                        return
-                if len(dnsresult) < 1:
-                        transaction['fail'] = "DNS response from %s for testpdns.com MX has no records" % transaction['host']
-                print "Result of checking domains:"
-                for rdata in dnsresult:
-                        print rdata.target
