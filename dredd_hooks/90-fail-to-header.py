85eebea473e00593d45781f47d0efb0eef398598
Maksim Kalashnikov <kalashnikovm@mail.ru>
2016-05-31 14:29:44 +0300

    remove header

--- dredd_hooks/90-fail-to-header.py
+++
@@ -1,9 +0,0 @@
-import dredd_hooks as hooks
-        
-# Add fail text to header
-@hooks.before_each
-def add_failtext_to_header(transaction):
-            if transaction['skip'] != True:
-                    if 'fail' in transaction:
-                            if transaction['fail'] != True :
-                                    transaction['request']['headers']['Dredd-Fail'] = transaction['fail']
