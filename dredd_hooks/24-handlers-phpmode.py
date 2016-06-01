38ddef9761f04a7b3a293fcdc488d2a415143d00
Maksim Kalashnikov <kalashnikovm@mail.ru>
2016-06-01 15:13:26 +0300

    test php fpm mode

--- dredd_hooks/24-handlers-phpmode.py
+++ dredd_hooks/24-handlers-phpmode.py
@@ -1,8 +1,35 @@
 import time
+import json
 import dredd_hooks as hooks
 
+# Run grab url function
+def run_url():
+        req = Request('http://test.com/phpjson.php')
+        try:
+                response = urlopen(req)
+        except HTTPError as e:
+                print 'The server couldn\'t fulfill the request.'
+                print 'Error code: ', e.code
+        except URLError as e:
+                print 'We failed to reach a server.'
+                print 'Reason: ', e.reason
+        return response
+
+# Check for nginx PHP FPM
+def check_nginx_php_fpm(j):
+        if j['SAPI'] != 'fpm-fcgi':
+                return False
+        if "nginx" not in j['Server']: 
+                return False
+
 # Checking PHP mode
-@hooks.after('FTP accounts > List FTP accounts > List FTP accounts')
+@hooks.before('Web Domains > List all web domains > List web domains')
 def check_php_mode(transaction):
         if transaction['skip'] != True:
-
+                try:
+                        j = json.loads(run_url())
+                except:
+                        transaction['fail'] = 'Cannot decode response to JSON'
+                
+                if check_nginx_php_fpm(j) == False:
+                        transaction['fail'] = 'Nginx PHP FPM test failed. Data is:'.join(j)
