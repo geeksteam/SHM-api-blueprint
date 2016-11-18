9f9f55be68554302b4a33be484391801588b6c7c
Maksim Kalashnikov <kalashnikovm@mail.ru>
2016-11-18 20:30:34 +0200

    gruntfile added

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+
+
+module.exports = function(grunt){
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+    var apibType = grunt.option('type') || '';
+    if ( apibType === '' ) {
+        apibType = 'all';
+    }
+
+    grunt.initConfig({
+        exec: {
+            // Remove tabs
+            removetabs: [ 'perl', '-p', '-i', '-e', '"s/\t/  /g"', 'apiary.apib' ].join(' '),
+            // exec Make
+            makeapib: [ './MakeApib', apibType, './apiary.apib' ].join(' ')
+        },
+        watch: {
+            scripts: {
+                files: ['**/*.apib'],
+                tasks: ['exec:makeapib','exec:removetabs'],
+                options: {
+                    spawn: false,
+                },
+            },
+        }
+    });
+    grunt.loadNpmTasks('grunt-env');
+    grunt.loadNpmTasks('grunt-exec');
+    grunt.registerTask('default', [ 'watch' ]);
+}
