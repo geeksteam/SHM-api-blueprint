42674f0770389219a76885d01772ac9aea1e47cd
Maksim Kalashnikov <kalashnikovm@mail.ru>
2016-11-22 19:56:06 +0200

    email hook test

--- email/dredd_hooks.py
+++ email/dredd_hooks.py
@@ -19,10 +19,10 @@ destination = ['kalashnikovm@mail.ru']
 PASSWORD = "infoPassword"
 
 # Wait until pop3 test
-wait_before_pop3 = 30
+wait_before_pop3 = 15
 
 
-@hooks.before_validation('Email boxes > #DreddHook pop3/smtp testing > #DreddHook pop3/smtp testing')
+@hooks.before_validation('Email boxes > @Hook pop3/smtp testing > @Hook pop3/smtp testing')
 def test_email(transaction):
         if transaction['skip'] != True:
 
@@ -43,69 +43,55 @@ def test_email(transaction):
 
                 This is a test e-mail message.
                 """ % testbox
-                try:
-                        smtpObj = smtplib.SMTP('localhost')
-                        smtpObj.sendmail(dreddsender, receivers, message)         
-                        print "Successfully sent email"
-                except:
-                        transaction["fail"] = "Error: unable to send email by Dredd smtp"
-                        return
+
+                smtpObj = smtplib.SMTP('localhost')
+                smtpObj.set_debuglevel(True)
+                smtpObj.sendmail(dreddsender, receivers, message)         
+                print "Successfully sent email"
+                smtpObj.quit()
                         
                 # Send email using SMTP AUTH
-                try:
-                        
-                        message = """From: Dredd <%s>
-                        To: <max@geeks.team>
-                        Subject: SMTP e-mail Dredd AUTH test
+                message = """From: Dredd <%s>
+                To: <max@geeks.team>
+                Subject: SMTP e-mail Dredd AUTH test
 
-                        This is a test e-mail message.
-                        """ % testbox
-
-                        conn = smtplib.SMTP(SMTPserver)
-                        conn.set_debuglevel(True)
-                        
-                        conn.login(USERNAME, PASSWORD)
-                        conn.sendmail(testbox, destination, message)
-                        
-                        conn.quit()
-
-                except Exception, exc:
-                        transaction["fail"] = "SMTP AUTH mail failed; %s" % str(exc)
-                        return
+                This is a test e-mail message.
+                """ % testbox
+                conn = smtplib.SMTP(SMTPserver)
+                conn.set_debuglevel(True)
+                
+                conn.login(USERNAME, PASSWORD)
+                conn.sendmail(testbox, destination, message)
+                conn.quit()
                         
 
-                # Wait 15 sec until message is proccesed by exim
+                # Wait until message is proccesed by exim
                 time.sleep(wait_before_pop3)
 
                 # Check POP3 message is in box
                 
                 box = poplib.POP3(SMTPserver)
-                try:
-                        box.user(USERNAME)
-                        box.pass_(PASSWORD)
+                box.user(USERNAME)
+                box.pass_(PASSWORD)
+
+                response, lst, octets = box.list()
+                print "DEBUG: Total messages: %s" % len(lst)
+                total_messages = len(lst)
                 
-                        response, lst, octets = box.list()
-                        print "DEBUG: Total messages: %s" % len(lst)
-                        total_messages = len(lst)
-                        
-                        message_found = False
-                        for msgnum, msgsize in [i.split() for i in lst]:
-                                (resp, lines, octets) = box.retr(msgnum)
-                                msgtext = "n".join(lines) + "nn"
-                                message = email.message_from_string(msgtext) 
-                                # Check for message from Dredd
-                                if "<dredd-test@geeks.team>" in message["from"] :
-                                        message_found = True
-                                print(msgtext)
-                                box.dele(msgnum)
-                        box.quit()
-                        
-                except:
-                        transaction["fail"] = "Dredd POP3 Client LOGIN or MESSAGE LIST/DELETE failed."
-                        return
+                message_found = False
+                for msgnum, msgsize in [i.split() for i in lst]:
+                        (resp, lines, octets) = box.retr(msgnum)
+                        msgtext = "n".join(lines) + "nn"
+                        message = email.message_from_string(msgtext) 
+                        # Check for message from Dredd
+                        if "<dredd-test@geeks.team>" in message["from"] :
+                                message_found = True
+                        print(msgtext)
+                        box.dele(msgnum)
+                box.quit()
                 
                 if message_found == False:
                         transaction["fail"] = "Dredd POP3 client failed: Message from DREDD not found in messages list. Total messages: %s" % total_messages
-                # # Set to success
+                # Set to success
                 transaction['real']['statusCode'] = 202
                 transaction['real']['body'] = ''
