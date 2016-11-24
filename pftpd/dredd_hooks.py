458552b805b643b3833661d6454f7de95185b66d
Maksim Kalashnikov <kalashnikovm@mail.ru>
2016-11-24 15:09:46 +0200

    #

--- pftpd/dredd_hooks.py
+++ pftpd/dredd_hooks.py
@@ -3,7 +3,7 @@ import ftplib
 import dredd_hooks as hooks
 
 # Test FTP login as created account
-@hooks.before('FTP accounts > !Hook FTP account testing')
+@hooks.before('FTP accounts > !Hook FTP account testing > !Hook FTP account testing')
 def check_ftp_account_test(transaction):
 		if transaction['skip'] != True:
 				print >> sys.stderr, 'Test FTP accounts Hook Started'
